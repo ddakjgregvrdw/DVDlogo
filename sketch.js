613f0cdd4831a03df8e19b3cf7777bddc143794d
ddakjgregvrdw <47510688+ddakjgregvrdw@users.noreply.github.com>
2019-02-16 16:34:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ let yspeed;
 let dvd;
 let dvdSize=innerWidth/6;
 let canvas;
+let cornerCounter=0
+let font, fontsize = 40;
 
 function preload() {
     dvd = loadImage("dvd.png");
@@ -14,13 +16,23 @@ function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
     y = 100;
     x = 100;
-    xspeed = 5;
-	yspeed = 5;
+    xspeed = dvdSize/50;
+	yspeed = dvdSize/50;
+}
+
+function cornerHit(){
+	cornerCounter++;
 }
 
 function draw() {
     background(0);
 	image(dvd, x, y, dvdSize, dvdSize);
+
+	if (mouseIsPressed){
+    	cornerHit();
+    }
+    fill(255);
+  	text(cornerCounter, width-dvdSize, height-dvdSize);
 
 	x = x + xspeed;
 	y = y + yspeed;
@@ -28,7 +40,7 @@ function draw() {
 	if (x >= width-dvdSize ) {
         xspeed = -xspeed;
         x = width-dvdSize;
-	}else if(x<=0){
+	}else if(x<=0){ 
 		xspeed = -xspeed;
 		x = 0;
 	}
@@ -39,6 +51,19 @@ function draw() {
 		yspeed = -yspeed;
 		y = 0;
 	}
+
+	//logic for corner hits 
+	if (x <= 0 && y <= 0) {
+        cornerHit();
+	}else if (x <= 0 || y >= height-dvdSize){ 
+		cornerHit();
+	}else if (x >= width-dvdSize && y <= 0) {
+		cornerHit();
+	}else if(x >= width-dvdSize &&  y >= height-divSize){
+		cornerHit();
+	}
+
+
 }
 
 window.onresize = function() {
